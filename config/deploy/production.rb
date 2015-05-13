2ef5641be950cdb2388b31b82404f6046703bcd1
Peter Mangiafico <peter@mangiafico.org>
2015-05-13 15:32:34 -0700

    remove robots3 server from prod deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,5 @@
 server 'sul-robots1-prod.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
 server 'sul-robots2-prod.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
-server 'sul-robots3-prod.stanford.edu', user: 'lyberadmin', roles: %w{web app db}
 
 Capistrano::OneTimeKey.generate_one_time_key!
 
