bec0cf46c0a1ff801267ac382cace7605723d0c7
Laney McGlohon <laneymcg@stanford.edu>
2015-05-19 08:11:54 -0700

    Completed implementation of the 856 based upon new requirements

--- lib/dor/update_marc_record_service.rb
+++ lib/dor/update_marc_record_service.rb
@@ -5,7 +5,7 @@ module Dor
     
     def initialize druid_obj
       @druid_obj = druid_obj
-      @druid_id = @druid_obj.id
+      @druid_id = @druid_obj.id.sub("druid:","")
     end
 
     def push_symphony_record
@@ -20,7 +20,7 @@ module Dor
       
       purl_uri = get_u_field
       collection_info = get_x2_collection_info
-      
+
       # catkey: the catalog key that associates a DOR object with a specific Symphony record.
       # .856. 41
       # Subfield u (required): the full Purl URL
@@ -35,14 +35,14 @@ module Dor
       # Subfield x #5 (required): the file-id to be used as thumb if available, else null
       # Subfield x #6..n (optional): Collection(s) this object is a member of, recorded as druid-value:ckey-value:title
 
-      return "#{catkey}\t#{get_856_cons} #{get_1st_indicator}#{get_2nd_indicator}#{purl_uri}#{get_x1_sdrpurl_marker}#{object_type}#{collection_info}"
+      return "#{catkey}\t#{get_856_cons} #{get_1st_indicator}#{get_2nd_indicator}#{purl_uri}#{get_x1_sdrpurl_marker}#{object_type}#{display_type}#{barcode}#{file_ids}#{collection_info}"
     end
     
     def write_symphony_record symphony_record
       if symphony_record.nil? || symphony_record.length == 0 then
         return
       end
-      symphony_file_name = "#{Dor::Config.release.symphony_path}/sdr-purl-#{@druid_id.sub("druid:","")}-#{Time.now.strftime('%Y%m%d%H%M%S')}"
+      symphony_file_name = "#{Dor::Config.release.symphony_path}/sdr-purl-#{@druid_id}-#{Time.now.strftime('%Y%m%d%H%M%S')}"
       command = "#{Dor::Config.release.write_marc_script} '#{symphony_record}' #{symphony_file_name}"
       run_write_script(command)
     end
@@ -65,11 +65,7 @@ module Dor
         catkey = nil
         node = identity_md.at_xpath("//identityMetadata/otherId[@name='catkey']")
         catkey = node.content if node
-        if !catkey
-          nil
-        else
-          catkey
-        end
+        catkey
       end
     end
 
@@ -77,16 +73,52 @@ module Dor
     # look in identityMetadata/objectType
     def object_type
       @objectType ||= begin
-        objectType = nil
+        objectType = ''
         node = identity_md.at_xpath("//identityMetadata/objectType")
         objectType = node.content if node
-        if !objectType
-          objectType = "|x"
-        else
-          objectType = "|x#{objectType}"
-        end
-        objectType
+        objectType.prepend("|x")
       end
+    end
+
+    # value is used to tell SearchWorks UI app of specific display needs for objects
+    # @return [String] identityMetadata displayType, DOR content type, or citation in an x subfield
+    def display_type
+      @displayType ||= begin
+        displayType = ''
+        if node = identity_md.at_xpath("//identityMetadata/displayType")
+          displayType = node.content
+        elsif node = content_md.at_xpath("//contentMetadata/@type")
+          displayType = node.content
+        else
+          if object_type != "|xcollection"
+            displayType = "citation"
+          end
+        end
+        displayType.prepend("|x")
+      end
+    end
+
+    # @return [String] value with barcode in it, or empty x subfield if none exists
+    # look in identityMetadata/otherId name="barcode"
+    def barcode
+      @barcode ||= begin
+        barcode = ''
+        node = identity_md.at_xpath("//identityMetadata/otherId[@name='barcode']")
+        barcode = node.content if node
+        barcode.prepend("|x")
+      end
+    end
+
+    # the @id attribute of resource/file elements that match the display_type, including extension
+    # @return [String] filenames separated by comma
+    def file_ids
+      ids = []
+      if content_md
+        content_md.xpath('//contentMetadata/resource/file').each { |node|
+          ids << node.attr("id") if !node.nil?
+        }
+      end
+      ids.join(',').prepend("|x")
     end
 
     # It returns 856 constants
@@ -111,7 +143,7 @@ module Dor
     
     # It builds the PURL uri based on the druid id
     def get_u_field 
-      return "|u#{Dor::Config.release.purl_base_uri}/#{@druid_id.sub("druid:","")}"
+      return "|u#{Dor::Config.release.purl_base_uri}/#{@druid_id}"
     end
 
     # It returns the SDR-PURL subfield
@@ -120,17 +152,18 @@ module Dor
     end
 
     # It returns the collection information subfields if exists
-    # @return [String] the colleciton information druid:value:title format
+    # @return [String] the colleciton information druid-value:catkey-value:title format
     def get_x2_collection_info 
       collections = @druid_obj.collections
+      coll_info = ""
 
       if collections.length > 0 then
-        collection_title = collections[0].label
-        collection_id = collections[0].id
-        return "|x#{collection_id}:#{collection_title}"
-      else
-        return ""
+        collections.each { |coll|
+          coll_info += "|x#{coll.id}:#{coll.catkey}:#{coll.label}"
+        }
       end
+
+      coll_info
     end
 
     # the identityMetadata for this object 
--- spec/lib/dor/update_marc_record_service_spec.rb
+++ spec/lib/dor/update_marc_record_service_spec.rb
@@ -17,47 +17,54 @@ describe Dor::UpdateMarcRecordService do
       item=double(Dor::Item.new)
       collection = double(Dor::Collection.new)
       identityMetadataXML = double(String)
-      
+
       allow(identityMetadataXML).to receive_messages(
         :ng_xml => Nokogiri::XML(build_identity_metadata_3)
       )
-      
+
       allow(collection).to receive_messages(
         :label => "Collection label",
-        :id => "druid:cc111cc1111",
+        :id => "cc111cc1111",
+        :catkey => "12345678"
       )
-      
+
       allow(item).to receive_messages(
-        :id => "druid:aa111aa1111",
+        :id => "aa111aa1111",
         :collections =>[collection],
-        :datastreams => {"identityMetadata"=>identityMetadataXML} 
+        :datastreams => {"identityMetadata"=>identityMetadataXML}
       )
       updater = Dor::UpdateMarcRecordService.new(item)
       expect(updater.generate_symphony_record).to eq("")
     end
-    it "should generate symphony record for a druid object with catkey" do
+    it "should generate symphony record for a item object with catkey" do
       Dor::Config.release.purl_base_uri = "http://purl.stanford.edu"
 
       item=double(Dor::Item.new)
       collection = double(Dor::Collection.new)
       identityMetadataXML = double(String)
-      
+      contentMetadataXML = double(String)
+
       allow(identityMetadataXML).to receive_messages(
         :ng_xml => Nokogiri::XML(build_identity_metadata_1)
       )
-      
+
+      allow(contentMetadataXML).to receive_messages(
+        :ng_xml => Nokogiri::XML(build_content_metadata_1)
+      )
+
       allow(collection).to receive_messages(
         :label => "Collection label",
-        :id => "druid:cc111cc1111",
+        :id => "cc111cc1111",
+        :catkey => nil
       )
-      
+
       allow(item).to receive_messages(
-        :id => "druid:aa111aa1111",
+        :id => "aa111aa1111",
         :collections =>[collection],
-        :datastreams => {"identityMetadata"=>identityMetadataXML} 
+        :datastreams => {"identityMetadata"=>identityMetadataXML, "contentMetadata"=>contentMetadataXML}
       )
       updater = Dor::UpdateMarcRecordService.new(item)
-      expect(updater.generate_symphony_record).to eq("8832162\t.856. 41|uhttp://purl.stanford.edu/aa111aa1111|xSDR-PURL|xitem|xdruid:cc111cc1111:Collection label")
+      expect(updater.generate_symphony_record).to eq("8832162\t.856. 41|uhttp://purl.stanford.edu/aa111aa1111|xSDR-PURL|xitem|ximage|x36105216275185|xwt183gy6220_00_0001.jp2|xcc111cc1111::Collection label")
     end
     
     it "should generate symphony record for a collection object with catkey" do
@@ -65,20 +72,25 @@ describe Dor::UpdateMarcRecordService do
 
       item=double(Dor::Item.new)
       identityMetadataXML = double(String)
+      contentMetadataXML = double(String)
       
       allow(identityMetadataXML).to receive_messages(
         :ng_xml => Nokogiri::XML(build_identity_metadata_2)
       )
-      
+
+      allow(contentMetadataXML).to receive_messages(
+        :ng_xml => Nokogiri::XML(build_content_metadata_2)
+      )
+
       allow(item).to receive_messages(
         :label => "Collection label",
-        :id => "druid:aa111aa1111",
+        :id => "aa111aa1111",
         :collections =>[],
-        :datastreams => {"identityMetadata"=>identityMetadataXML} 
+        :datastreams => {"identityMetadata"=>identityMetadataXML, "contentMetadata"=>contentMetadataXML}
        )
      
       updater = Dor::UpdateMarcRecordService.new(item)
-      expect(updater.generate_symphony_record).to eq("8832162\t.856. 41|uhttp://purl.stanford.edu/aa111aa1111|xSDR-PURL|xcollection")
+      expect(updater.generate_symphony_record).to eq("8832162\t.856. 41|uhttp://purl.stanford.edu/aa111aa1111|xSDR-PURL|xcollection|x|x|xwt183gy6220_00_0001.jp2,wt183gy6220_00_0002.jp2")
     end
   end
 
@@ -86,7 +98,7 @@ describe Dor::UpdateMarcRecordService do
     xit "should write the symphony record to the symphony directory" do
       d = Dor::Item.new 
       updater = Dor::UpdateMarcRecordService.new(d)
-      updater.instance_variable_set(:@druid_id,"druid:aa111aa1111")
+      updater.instance_variable_set(:@druid_id,"aa111aa1111")
       Dor::Config.release.symphony_path = "#{@fixtures}/sdr_purl"
       Dor::Config.release.write_marc_script = "bin/write_marc_record_test"
       updater.write_symphony_record "aaa"
@@ -97,7 +109,7 @@ describe Dor::UpdateMarcRecordService do
     it "should do nothing if the symphony record is empty" do
       d = Dor::Item.new 
       updater = Dor::UpdateMarcRecordService.new(d)
-      updater.instance_variable_set(:@druid_id,"druid:aa111aa1111")
+      updater.instance_variable_set(:@druid_id,"aa111aa1111")
       Dor::Config.release.symphony_path = "#{@fixtures}/sdr_purl"
       updater.write_symphony_record ""
       
@@ -107,7 +119,7 @@ describe Dor::UpdateMarcRecordService do
     it "should do nothing if the symphony record is nil" do
       d = Dor::Item.new 
       updater = Dor::UpdateMarcRecordService.new(d)
-      updater.instance_variable_set(:@druid_id,"druid:aa111aa1111")
+      updater.instance_variable_set(:@druid_id,"aa111aa1111")
       Dor::Config.release.symphony_path = "#{@fixtures}/sdr_purl"
       updater.write_symphony_record ""
     
@@ -158,22 +170,121 @@ describe Dor::UpdateMarcRecordService do
   describe ".object_type" do
     it "should return object_type from a valid identityMetadata" do
       identityMetadataXML = double(String)
-      
+
       allow(identityMetadataXML).to receive_messages(
         :ng_xml => Nokogiri::XML(build_identity_metadata_1)
       )
-      
-      d = Dor::Item.new 
+
+      d = Dor::Item.new
 
       allow(d).to receive_messages(
-        :datastreams => {"identityMetadata"=>identityMetadataXML} 
+        :datastreams => {"identityMetadata"=>identityMetadataXML}
       )
 
       updater = Dor::UpdateMarcRecordService.new(d)
       expect(updater.object_type).to eq("|xitem")
     end
     
-    it "should return an empty x subfield for an identityMetadata without object_type" do
+    it "should return an empty x subfield for identityMetadata without object_type" do
+      identityMetadataXML = double(String)
+
+      allow(identityMetadataXML).to receive_messages(
+        :ng_xml => Nokogiri::XML(build_identity_metadata_3)
+      )
+
+      d = Dor::Item.new
+
+      allow(d).to receive_messages(
+        :datastreams => {"identityMetadata"=>identityMetadataXML}
+      )
+
+      updater = Dor::UpdateMarcRecordService.new(d)
+      expect(updater.object_type).to eq("|x")
+    end
+  end
+
+  describe ".display_type" do
+    it "should return display_type from valid identityMetadata" do
+      identityMetadataXML = double(String)
+
+      allow(identityMetadataXML).to receive_messages(
+        :ng_xml => Nokogiri::XML(build_identity_metadata_1)
+      )
+
+      d = Dor::Item.new
+
+      allow(d).to receive_messages(
+        :datastreams => {"identityMetadata"=>identityMetadataXML}
+      )
+
+      updater = Dor::UpdateMarcRecordService.new(d)
+      expect(updater.display_type).to eq("|ximage")
+    end
+
+    it "should return contentMetadata @type for display_type if identityMetadata doesn't have display_type" do
+      identityMetadataXML = double(String)
+      contentMetadataXML = double(String)
+
+      allow(identityMetadataXML).to receive_messages(
+        :ng_xml => Nokogiri::XML(build_identity_metadata_3)
+      )
+
+      allow(contentMetadataXML).to receive_messages(
+        :ng_xml => Nokogiri::XML(build_content_metadata_1)
+      )
+
+      d = Dor::Item.new
+
+      allow(d).to receive_messages(
+        :datastreams => {"identityMetadata"=>identityMetadataXML, "contentMetadata"=>contentMetadataXML}
+      )
+
+      updater = Dor::UpdateMarcRecordService.new(d)
+      expect(updater.display_type).to eq("|xmap")
+    end
+
+    it "should return display_type of citation if identityMetadata doesn't have display_type and contentMetadata doesn't have @type" do
+      identityMetadataXML = double(String)
+      contentMetadataXML = double(String)
+
+      allow(identityMetadataXML).to receive_messages(
+        :ng_xml => Nokogiri::XML(build_identity_metadata_3)
+      )
+
+      allow(contentMetadataXML).to receive_messages(
+        :ng_xml => Nokogiri::XML(build_content_metadata_2)
+      )
+
+      d = Dor::Item.new
+
+      allow(d).to receive_messages(
+        :datastreams => {"identityMetadata"=>identityMetadataXML, "contentMetadata"=>contentMetadataXML}
+      )
+
+      updater = Dor::UpdateMarcRecordService.new(d)
+      expect(updater.display_type).to eq("|xcitation")
+    end
+  end
+
+  describe ".barcode" do
+    it "should return barcode from a valid identityMetadata" do
+      identityMetadataXML = double(String)
+
+      allow(identityMetadataXML).to receive_messages(
+        :ng_xml => Nokogiri::XML(build_identity_metadata_1)
+      )
+
+      d = Dor::Item.new
+
+      allow(d).to receive_messages(
+        :datastreams => {"identityMetadata"=>identityMetadataXML}
+      )
+
+      updater = Dor::UpdateMarcRecordService.new(d)
+      expect(updater.barcode).to eq("|x36105216275185")
+    end
+
+    it "should return an empty x subfield for identityMetadata without barcode" do
       identityMetadataXML = double(String)
       
       allow(identityMetadataXML).to receive_messages(
@@ -188,6 +299,42 @@ describe Dor::UpdateMarcRecordService do
 
       updater = Dor::UpdateMarcRecordService.new(d)
       expect(updater.object_type).to eq("|x")
+    end
+  end
+
+  describe ".file_ids" do
+    it "should return file_ids from a valid contentMetadata" do
+      contentMetadataXML = double(String)
+
+      allow(contentMetadataXML).to receive_messages(
+        :ng_xml => Nokogiri::XML(build_content_metadata_1)
+      )
+
+      d = Dor::Item.new
+
+      allow(d).to receive_messages(
+        :datastreams => {"contentMetadata"=>contentMetadataXML}
+      )
+
+      updater = Dor::UpdateMarcRecordService.new(d)
+      expect(updater.file_ids).to eq("|xwt183gy6220_00_0001.jp2")
+    end
+
+    it "should return an empty x subfield for contentMetadata without file_ids" do
+      contentMetadataXML = double(String)
+
+      allow(contentMetadataXML).to receive_messages(
+        :ng_xml => Nokogiri::XML(build_content_metadata_3)
+      )
+
+      d = Dor::Item.new
+
+      allow(d).to receive_messages(
+        :datastreams => {"contentMetadata"=>contentMetadataXML}
+      )
+
+      updater = Dor::UpdateMarcRecordService.new(d)
+      expect(updater.file_ids).to eq("|x")
     end
   end
 
@@ -219,7 +366,7 @@ describe Dor::UpdateMarcRecordService do
     it "should return valid purl url" do
       d = Dor::Item.new 
       updater = Dor::UpdateMarcRecordService.new(d)
-      updater.instance_variable_set(:@druid_id,"druid:aa111aa1111")
+      updater.instance_variable_set(:@druid_id,"aa111aa1111")
       Dor::Config.release.purl_base_uri = "http://purl.stanford.edu"
       expect(updater.get_u_field).to eq("|uhttp://purl.stanford.edu/aa111aa1111")
     end
@@ -252,15 +399,16 @@ describe Dor::UpdateMarcRecordService do
       
       allow(collection).to receive_messages(
         :label => "Collection label",
-        :id => "druid:cc111cc1111",
+        :id => "cc111cc1111",
+        :catkey => "12345678"
       )
       
       allow(item).to receive_messages(
-        :id => "druid:aa111aa1111",
+        :id => "aa111aa1111",
         :collections =>[collection],
       )
       updater = Dor::UpdateMarcRecordService.new(item)
-      expect(updater.get_x2_collection_info).to eq("|xdruid:cc111cc1111:Collection label")
+      expect(updater.get_x2_collection_info).to eq("|xcc111cc1111:12345678:Collection label")
     end
   end
   
@@ -271,6 +419,7 @@ describe Dor::UpdateMarcRecordService do
   <objectCreator>DOR</objectCreator>
   <objectLabel>A  new map of Africa</objectLabel>
   <objectType>item</objectType>
+  <displayType>image</displayType>
   <adminPolicy>druid:dd051ys2703</adminPolicy>
   <otherId name="catkey">8832162</otherId>
   <otherId name="barcode">36105216275185</otherId>
@@ -291,7 +440,6 @@ describe Dor::UpdateMarcRecordService do
   <objectType>collection</objectType>
   <adminPolicy>druid:dd051ys2703</adminPolicy>
   <otherId name="catkey">8832162</otherId>
-  <otherId name="barcode">36105216275185</otherId>
   <otherId name="uuid">ff3ce224-9ffb-11e3-aaf2-0050569b3c3c</otherId>
   <tag>Process : Content Type : Map</tag>
   <tag>Project : Batchelor Maps : Batch 1</tag>
@@ -300,14 +448,13 @@ describe Dor::UpdateMarcRecordService do
   <tag>Remediated By : 4.15.4</tag>
 </identityMetadata>'
   end
-    def build_identity_metadata_3
+  def build_identity_metadata_3
     identityMetadataXML = '<identityMetadata>
   <sourceId source="sul">36105216275185</sourceId>
   <objectId>druid:bb987ch8177</objectId>
   <objectCreator>DOR</objectCreator>
   <objectLabel>A  new map of Africa</objectLabel>
   <adminPolicy>druid:dd051ys2703</adminPolicy>
-  <otherId name="barcode">36105216275185</otherId>
   <otherId name="uuid">ff3ce224-9ffb-11e3-aaf2-0050569b3c3c</otherId>
   <tag>Process : Content Type : Map</tag>
   <tag>Project : Batchelor Maps : Batch 1</tag>
@@ -317,4 +464,34 @@ describe Dor::UpdateMarcRecordService do
 </identityMetadata>'
   end
 
+  def build_content_metadata_1
+    contentMetadataXML = '<contentMetadata objectId="wt183gy6220" type="map">
+<resource id="wt183gy6220_1" sequence="1" type="image">
+<label>Image 1</label>
+<file id="wt183gy6220_00_0001.jp2" mimetype="image/jp2" size="3182927">
+<imageData width="4531" height="3715"/>
+</file>
+</resource>
+</contentMetadata>'
+  end
+  def build_content_metadata_2
+    contentMetadataXML = '<contentMetadata objectId="wt183gy6220">
+<resource id="wt183gy6220_1" sequence="1" type="image">
+<label>Image 1</label>
+<file id="wt183gy6220_00_0001.jp2" mimetype="image/jp2" size="3182927">
+<imageData width="4531" height="3715"/>
+</file>
+</resource>
+<resource id="wt183gy6220_2" sequence="2" type="image">
+<label>Image 2</label>
+<file id="wt183gy6220_00_0002.jp2" mimetype="image/jp2" size="3182927">
+<imageData width="4531" height="3715"/>
+</file>
+</resource>
+</contentMetadata>'
+  end
+    def build_content_metadata_3
+    contentMetadataXML = '<contentMetadata objectId="wt183gy6220">
+</contentMetadata>'
+  end
 end
